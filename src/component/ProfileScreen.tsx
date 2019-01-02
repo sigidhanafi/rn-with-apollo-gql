cadbdbf02447f29897105a72dfcad2c98d03c67e
sigidhanafi <sigidhanafi@gmail.com>
2019-01-02 18:53:20 +0700

    handle error

--- src/component/ProfileScreen.tsx
+++ src/component/ProfileScreen.tsx
@@ -1,11 +1,24 @@
 import React from 'react'
-import { View, Text, ActivityIndicator, Image } from 'react-native'
+import { View, Text, ActivityIndicator, Image, TouchableOpacity } from 'react-native'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 
 class ProfileScreen extends React.Component {
+  handleRetry = () => {
+    const { data } = this.props
+    const { refetch } = data
+    console.log('halo', data)
+
+    try {
+      refetch()
+    } catch (e) {
+      console.log('Error', e)
+    }
+  }
+
   render() {
     const { data } = this.props
+    console.log('DARA', data)
     if (data && data.loading) {
       return (
         <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
@@ -16,15 +29,18 @@ class ProfileScreen extends React.Component {
 
     if (data && data.error) {
       return (
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-          <Text>Failed to get data.</Text>
+        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, margin: 20 }}>
+          <Text style={{ textAlign: 'center' }}>{data.error.message}</Text>
+          <TouchableOpacity onPress={this.handleRetry}>
+            <Text>Coba lagi</Text>
+          </TouchableOpacity>
         </View>
       )
     }
 
     if (data && data.user) {
       return (
-        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1, margin: 20}}>
           <Image source={{ uri: data.user.avatarUrl }} style={{ width: 100, height: 100, margin: 10 }}/>
           <Text>{ data.user.name  }</Text>
           <Text>{ data.user.bio  }</Text>
